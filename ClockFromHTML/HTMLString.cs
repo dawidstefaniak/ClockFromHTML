e6c29b6ad83c48e4db243915283f71382de42aed
DawidStefaniak <contact@dawidstefaniak.com>
2017-11-19 15:47:20 +0000

    Added class to read HTML

---
+++ ClockFromHTML/HTMLString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace ClockFromHTML
+{
+    class HTMLString
+    {
+
+    public StreamReader ReadHTMLStream(string urlAddress)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader sr = null;
+                if (response.CharacterSet != null)
+                {
+                    sr = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                }
+                return sr;
+            }
+            else return null;
+        }
+    }
+}
--- ClockFromHTML/Program.cs
+++ ClockFromHTML/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace ClockFromHTML
 {
@@ -10,7 +12,14 @@ namespace ClockFromHTML
     {
         static void Main(string[] args)
         {
-            d
+            HTMLString htmlreader = new HTMLString();
+            StreamReader sr = htmlreader.ReadHTMLStream("https://time.is");
+            using (sr)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            Console.ReadKey();
         }
+
     }
 }
